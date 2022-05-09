e3a66cde11e8975aa6a6dda3c0710f062b3f69d0
Victor Queiroz <victoorqrz@gmail.com>
2022-05-09 16:55:38 -0300

    creating feedback and by email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,40 @@
 import express from 'express'
+import { prisma } from './prisma'
+import nodemailer from 'nodemailer'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-  return res.send('hello')
+app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '33fe55c1064e8d',
+    pass: '701ff2cbe69651'
+  }
+})
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <victor@bonsae.com>',
+    to: 'victor <victoorqrz@gmail.com>',
+    subject: 'novo feedback',
+    html: `<p>Tipo do feeback: ${type}</p>
+    <p>Comentario: ${comment}</p>`
+  })
+
+  return res.status(201).json({ data: feedback })
 })
 
 app.listen(3333, () => {
