404ffee67469d66577ad60c02f44758394519041
Victor Queiroz <victoorqrz@gmail.com>
2022-05-09 15:14:17 -0300

    allowing screenshots

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from 'phosphor-react'
+import { useState } from 'react'
 import { FeedBackType, feedBackTypes } from '..'
 import { CloseButton } from '../../CloseButton'
+import { ScreenshotButton } from '../ScreenshotButton'
 
 interface FeedBackContentStepProps {
   feedbackType: FeedBackType
@@ -11,6 +13,8 @@ export function FeedBackContentStep({
   feedbackType,
   onFeedbackRestartRequested
 }: FeedBackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+
   const feedbackTypeInfo = feedBackTypes[feedbackType]
 
   return (
@@ -42,12 +46,10 @@ export function FeedBackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
-          <button
-            type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6" />
-          </button>
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
